1d451ee20960189bea27bb3134eb327a29f7b988
DAV215 <clone21052001@gmail.com>
2024-01-09 02:00:19 +0700

    update sanphamnoibat

--- asset/js/menu.js
+++ asset/js/menu.js
@@ -2,15 +2,21 @@ document.addEventListener("scroll", function() {
     var menu = document.getElementById("menu");
     var menuMb = document.getElementById("menu_mb");
     var contentMenuMobile = document.getElementById("content_menu_moblie");
-
+    var sidebar_main = document.querySelector('.sidebar_main');
     if (window.scrollY >= 300) {
         menu.classList.add("fixed");
         menuMb.classList.add("fixed");
         contentMenuMobile.classList.add("fixed");
+        sidebar_main.style.position = 'fixed';
+        sidebar_main.style.top = menu.offsetTop + 70 + 'px';
+
     } else {
         menu.classList.remove("fixed");
         menuMb.classList.remove("fixed");
         contentMenuMobile.classList.remove("fixed");
+        sidebar_main.style.position = 'relative';
+        sidebar_main.style.top = -15 + 'px';
+
     }
 });
 
--- pages/sidebar/sidebar.php
+++ pages/sidebar/sidebar.php
@@ -4,22 +4,25 @@
     $query_lietke__sidebar = mysqli_query($mysqli, $sql_lietke__sidebar);
 ?>
 <div class="sidebar">
-    <div class="sidebar-name">
-        <h1>Danh mục sản phẩm </h1>
+    <div class="sidebar_main">
+        <div class="sidebar-name">
+            <h1>Danh mục sản phẩm </h1>
+        </div>
+        <div class="sidebar-content">
+            <ul> <!-- Added <ul> tag to wrap list items -->
+                <?php
+                while ($row_lietke__sidebar = mysqli_fetch_array($query_lietke__sidebar)) {
+                ?>
+                    <li>
+                        <a href="index.php?quanly=danhmucsanpham&id=<?php echo $row_lietke__sidebar['tendanhmuc'] ?>">
+                            <?php echo $row_lietke__sidebar['tendanhmuc'] ?>
+                        </a>
+                    </li>
+                <?php
+                }
+                ?>
+            </ul>
+        </div>
     </div>
-    <div class="sidebar-content">
-        <ul> <!-- Added <ul> tag to wrap list items -->
-            <?php
-            while ($row_lietke__sidebar = mysqli_fetch_array($query_lietke__sidebar)) {
-            ?>
-                <li>
-                    <a href="index.php?quanly=danhmucsanpham&id=<?php echo $row_lietke__sidebar['tendanhmuc'] ?>">
-                        <?php echo $row_lietke__sidebar['tendanhmuc'] ?>
-                    </a>
-                </li>
-            <?php
-            }
-            ?>
-        </ul>
-    </div>
+
 </div>
